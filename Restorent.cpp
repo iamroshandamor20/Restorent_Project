69742164e804411eb319afaf0d8657d0833ce4f5
Roshan Damor <iamroshandamor@gmail.com>
2024-07-06 17:03:51 +0530

    Updated code

--- Restorent.cpp
+++ Restorent.cpp
@@ -1,153 +1,105 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-class Restorent
-{
-    float r,bill;
+class Restaurant {
+    float r, bill;
 public:
-void order(int qnt)
-{
-    r=100;
-    bill=r*qnt;
-    cout<<"You Chooseed "<<qnt<<" Sadi Thali and Your Bill is : "<<bill<<endl;
-}
-void order(int qnt)
-{
-    r=200;
-    bill=r*qnt;
-    cout<<"You Chooseed "<<qnt<<" Deluxe Thali and Your Bill is : "<<bill<<endl;
-}
-void order(int qnt, char mdish[25])
-{
-     cout<<"\t Menu"<<endl;
-
-    do
-    {
-        cout<<"Dal Tadka\t Rs 50 \t Press 1"<<endl;
-        cout<<"Dal Fry\t Rs 50 \t Press 1"<<endl;
-        cout<<"Dal Makhani\t Rs 50 \t Press 1"<<endl<<endl;
-        cout<<"Shahi Paneer\t Rs 60 \t Press 1"<<endl;
-        cout<<"Kadai Paneer\t Rs 60 \t Press 1"<<endl;
-        cout<<"Masala Paneer\t Rs 60 \t Press 1"<<endl<<endl;
-        cout<<"Chana Masala\t Rs 40 \t Press 1"<<endl;
-        cout<<"Malai Kofta\t Rs 40 \t Press 1"<<endl;
-        cout<<"Aloo Jeera\t Rs 40 \t Press 1"<<endl;
-
-        cin>>int ch;
-        switch (ch)
-        {
-        case 1:
-            r=50;
-            bill=qty*r;
-            break;
-        case 2:
-            r=50;
-            bill=qty*r;
-            break;
-        case 3:
-            r=50;
-            bill=qty*r;
-            break;
-        case 4:
-            r=40;
-            bill=qty*r;
-            break;
-        case 5:
-            r=60;
-            bill=qty*r;
-            break;
-        case 6:
-            r=60;
-            bill=qty*r;
-            break;
-        case 7:
-            r=60;
-            bill=qty*r;
-            break;
-        case 8:
-            r=40;
-            bill=qty*r;
-            break;
-        case 9:
-            r=40;
-            bill=qty*r;
-            break;
-        
-        default: cout<<"Choose Betweeen Oftions Only..."
-            break;
+    void order(int qnt, string type = "Sadi") {
+        if (type == "Sadi") {
+            r = 100;
+        } else if (type == "Deluxe") {
+            r = 200;
+        } else {
+            cout << "Invalid thali type." << endl;
+            return;
         }
-        cout<<"Press Y to Exit and Press Any Key To Continue...."<<endl;
-        cin>>char choo;
-    }while (choo!='y' || choo!='Y');  
-}
-void order(int qnt, char sdish[25])
-{
-     cout<<"\t Menu"<<endl;
+        bill = r * qnt;
+        cout << "You chose " << qnt << " " << type << " Thali and your bill is: " << bill << endl;
+    }
 
-    do
-    {
-        cout<<"Normal Burger\t Rs 49 \t Press 1"<<endl;
-        cout<<"Cheese Burger\t Rs 59 \t Press 1"<<endl;
-        cout<<"Munees Burger\t Rs 59 \t Press 1"<<endl<<endl;
-        cout<<"Corn Pizza\t Rs 199 \t Press 1"<<endl;
-        cout<<"Chicken Pizza\t Rs 199 \t Press 1"<<endl;
-        cout<<"Vegei Pizza\t Rs 199 \t Press 1"<<endl<<endl;
-        cout<<"Moroccan Spice Pasta \t Rs 139 \t Press 1"<<endl;
-        cout<<"Jalapano Cheese Pasta\t Rs 149 \t Press 1"<<endl;
-        cout<<"Tomato Creamy Pasta\t Rs 149 \t Press 1"<<endl;
+    void order(int qnt, char mdish[]) {
+        cout << "\tMenu" << endl;
+        cout << "1. Dal Tadka\t Rs 50" << endl;
+        cout << "2. Dal Fry\t Rs 50" << endl;
+        cout << "3. Dal Makhani\t Rs 50" << endl;
+        cout << "4. Shahi Paneer\t Rs 60" << endl;
+        cout << "5. Kadai Paneer\t Rs 60" << endl;
+        cout << "6. Masala Paneer\t Rs 60" << endl;
+        cout << "7. Chana Masala\t Rs 40" << endl;
+        cout << "8. Malai Kofta\t Rs 40" << endl;
+        cout << "9. Aloo Jeera\t Rs 40" << endl;
 
-        cin>>int ch;
-        switch (ch)
-        {
-        case 1:
-            r=50;
-            bill=qty*r;
-            break;
-        case 2:
-            r=50;
-            bill=qty*r;
-            break;
-        case 3:
-            r=50;
-            bill=qty*r;
-            break;
-        case 4:
-            r=40;
-            bill=qty*r;
-            break;
-        case 5:
-            r=60;
-            bill=qty*r;
-            break;
-        case 6:
-            r=60;
-            bill=qty*r;
-            break;
-        case 7:
-            r=60;
-            bill=qty*r;
-            break;
-        case 8:
-            r=40;
-            bill=qty*r;
-            break;
-        case 9:
-            r=40;
-            bill=qty*r;
-            break;
-        
-        default: cout<<"Choose Betweeen Oftions Only..."
-            break;
+        int ch;
+        cout << "Enter your choice: ";
+        cin >> ch;
+
+        switch (ch) {
+            case 1: case 2: case 3:
+                r = 50;
+                break;
+            case 4: case 5: case 6:
+                r = 60;
+                break;
+            case 7: case 8: case 9:
+                r = 40;
+                break;
+            default:
+                cout << "Choose between options only." << endl;
+                return;
         }
-        cout<<"Press Y to Exit and Press Any Key To Continue...."<<endl;
-        cin>>char choo;
-    }while (choo!='y' || choo!='Y');  
 
+        bill = qnt * r;
+        cout << "You chose " << qnt << " " << mdish << " and your bill is: " << bill << endl;
+    }
 
-}ob;
+    void order(int qnt, const char sdish[]) {
+        cout << "\tMenu" << endl;
+        cout << "1. Normal Burger\t Rs 49" << endl;
+        cout << "2. Cheese Burger\t Rs 59" << endl;
+        cout << "3. Munees Burger\t Rs 59" << endl;
+        cout << "4. Corn Pizza\t Rs 199" << endl;
+        cout << "5. Chicken Pizza\t Rs 199" << endl;
+        cout << "6. Veggie Pizza\t Rs 199" << endl;
+        cout << "7. Moroccan Spice Pasta\t Rs 139" << endl;
+        cout << "8. Jalapeno Cheese Pasta\t Rs 149" << endl;
+        cout << "9. Tomato Creamy Pasta\t Rs 149" << endl;
 
-int main()
-{
-    ob.order(5);
+        int ch;
+        cout << "Enter your choice: ";
+        cin >> ch;
+
+        switch (ch) {
+            case 1:
+                r = 49;
+                break;
+            case 2: case 3:
+                r = 59;
+                break;
+            case 4: case 5: case 6:
+                r = 199;
+                break;
+            case 7:
+                r = 139;
+                break;
+            case 8: case 9:
+                r = 149;
+                break;
+            default:
+                cout << "Choose between options only." << endl;
+                return;
+        }
+
+        bill = qnt * r;
+        cout << "You chose " << qnt << " " << sdish << " and your bill is: " << bill << endl;
+    }
+};
+
+int main() {
+    Restaurant ob;
+    ob.order(5, "Sadi");
+    ob.order(3, "Deluxe");
+    ob.order(2, "Paneer Dish");
+    ob.order(1, "Pasta Dish");
+    return 0;
 }
